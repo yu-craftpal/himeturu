e09e5433d3b447a1b3c2b3b69a6e553a69d87262
Hashimoto <you6673iy@outlook.jp>
2016-12-06 16:44:32 +0900

    起動時の動作を作成途中

--- himeturu/wing.cpp
+++ himeturu/wing.cpp
@@ -1,5 +1,43 @@
 #include "wing.h"
 
-HimeturuWing::HimeturuWing() {
-	
+HimeturuWing::HimeturuWing(wingservo l, wingservo r) {
+	winglsv = l;
+	wingrsv = r;
+	ics.begin();
+}
+
+void HimeturuWing::wakeup() {
+	int fls, flee, flde, frs, free, frde;
+	fls = flee = flde = frs = free = frde = 0;
+
+	winglpos.spoiler = ics.position(0, winglsv.spoiler);
+	winglpos.elebon_e = ics.position(0, winglsv.elebon_el);
+	if (winglsv.elebon_el != winglsv.elebon_er) {
+		winglpos.elebon_e = ics.position(0, winglsv.elebon_er);
+	}
+	winglpos.elebon_d = ics.position(0, winglsv.elebon_dl);
+	if (winglsv.elebon_dl != winglsv.elebon_dr) {
+		winglpos.elebon_d = ics.position(0, winglsv.elebon_dr);
+	}
+
+	wingrpos.spoiler = ics.position(0, wingrsv.spoiler);
+	wingrpos.elebon_e = ics.position(0, wingrsv.elebon_el);
+	if (winglsv.elebon_el != winglsv.elebon_er) {
+		wingrpos.elebon_e = ics.position(0, wingrsv.elebon_er);
+	}
+	wingrpos.elebon_d = ics.position(0, wingrsv.elebon_dl);
+	if (winglsv.elebon_dl != winglsv.elebon_dr) {
+		wingrpos.elebon_d = ics.position(0, wingrsv.elebon_dr);
+	}
+
+}
+
+void HimeturuWing::setSpoiler(axis ax) {
+
+
+
+}
+
+void HimeturuWing::setElebon(axis ax) {
+
 }
